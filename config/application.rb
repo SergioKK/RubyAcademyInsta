23899229b3e96f86a77d128c028ef22f93f41ecd
sergio <khotsenkos1993k@gmail.com>
2024-03-25 16:51:34 +0300

    Default settings

---
+++ config/application.rb
@@ -0,0 +1,43 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Insta
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # config for generating tests
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework(
+        :rspec,
+        fixtures: false,
+        view_spec: false,
+        helper_spec: false,
+        routing_spec: false
+      )
+    end
+
+    config.generators do |g|
+      g.factory_bot suffix: "factory"
+    end
+  end
+end
