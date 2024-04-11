526931ba9f2db5c3e60338adc080a199314abfdc
sergio <khotsenkos1993k@gmail.com>
2024-04-11 12:07:15 +0300

    Two ways of uploading image to rspec post factory.

--- spec/factories/posts_factory.rb
+++ spec/factories/posts_factory.rb
@@ -2,9 +2,14 @@ require "#{Rails.root}/spec/helpers/image_helper_spec.rb"
 
 FactoryBot.define do
   factory :post do
+    cached_file = Shrine.upload(File.open("spec/files/image.jpeg"), :cache)
+    cached_file.metadata["mime_type"] = "image/jpeg"
+    image_json = cached_file.to_json
+
     title { FFaker::Lorem.word }
     content { FFaker::Lorem.paragraph }
-    image_data { TestData.image_data }
+    # image { image_json }
+    image { TestData.image_data }
     user { FactoryBot.create(:user) }
   end
 end
--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -5,25 +5,18 @@ module TestData
     attacher = Shrine::Attacher.new
     attacher.set(uploaded_image)
 
-    # if you're processing derivatives
-    # attacher.set_derivatives(
-    #   large: uploaded_image,
-    #   medium: uploaded_image,
-    #   small: uploaded_image,
-    # )
-
-    attacher.column_data # or attacher.data in case of postgres jsonb column
+    attacher.column_data
   end
 
   def uploaded_image
-    file = File.open("spec/files/image.jpg", binmode: true)
+    file = File.open("spec/files/image.jpeg", binmode: true)
 
     # for performance we skip metadata extraction and assign test metadata
-    uploaded_file = Shrine.upload(file, :store, metadata: false)
+    uploaded_file = Shrine.upload(file, :cache, metadata: false)
     uploaded_file.metadata.merge!(
       "size" => File.size(file.path),
       "mime_type" => "image/jpeg",
-      "filename" => "image.jpg",
+      "filename" => "image.jpeg",
     )
 
     uploaded_file
