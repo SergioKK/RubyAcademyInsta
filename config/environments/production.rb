ffaa4e613fa04df9fc95fd16833627a4f4fb0896
sergio <khotsenkos1993k@gmail.com>
2024-04-15 17:59:22 +0300

    Change config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << ENV['DEVELOPMENT_HOSTS']
+  config.host<<"insta-clone-production.up.railway.app"
 end
